c39d146add50c3e5a436852e1daecc1d0580a940
Benjamin Paredes Brain <benja2005pb@gmail.com>
2023-12-20 17:19:33 -0300

    add: SEO (meta)

--- src/Components/Home/ServiciosHome.jsx
+++ src/Components/Home/ServiciosHome.jsx
@@ -1,37 +1,45 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const ServiciosHome = () => {
+    const navigate = useNavigate();
+
+    const scrollToSection = (sectionId) => {
+        navigate('/servicios');
+
+        setTimeout(() => {
+            const sectionContainer = document.getElementById(sectionId);
+
+            if (sectionContainer) {
+                sectionContainer.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'
+                });
+            }
+        }, 100);
+    };
 
     return (
         <div className="serviciosHome_mainContainer">
             <h4 className="serviciosHome_title">Contamos con todo tipo de servicios</h4>
 
             <div className="servicios_home_container">
-                <div className="servicios_home_imgContainer">
-                    <Link to={"/servicios"}>
-                        <img src="./alquiler.jpg" alt="alquiler inmobiliario" className="servicios_home_img" />
-                        <span className="imgText">Alquileres</span>
-                    </Link>
+                <div className="servicios_home_imgContainer" onClick={() => scrollToSection('alquileres')}>
+                    <img src="./alquiler.jpg" alt="alquiler inmobiliario" className="servicios_home_img" />
+                    <span className="imgText">Alquileres</span>
                 </div>
-                <div className="servicios_home_imgContainer">
-                    <Link to={"/servicios"}>
-                        <img src="./venta.jpg" alt="venta inmobiliaria" className="servicios_home_img" />
-                        <span className="imgText">Ventas</span>
-                    </Link>
+                <div className="servicios_home_imgContainer" onClick={() => scrollToSection('ventas')}>
+                    <img src="./venta.jpg" alt="venta inmobiliaria" className="servicios_home_img" />
+                    <span className="imgText">Ventas</span>
                 </div>
-                <div className="servicios_home_imgContainer">
-                    <Link to={"/servicios"}>
-                        <img src="./tasacion.jpg" alt="tasacion inmobiliaria" className="servicios_home_img" />
-                        <span className="imgText">Tasaciones</span>
-                    </Link>
+                <div className="servicios_home_imgContainer" onClick={() => scrollToSection('tasaciones')}>
+                    <img src="./tasacion.jpg" alt="tasacion inmobiliaria" className="servicios_home_img" />
+                    <span className="imgText">Tasaciones</span>
                 </div>
-                <div className="servicios_home_imgContainer">
-                    <Link to={"/servicios"}>
-                        <img src="./contrato.jpg" alt="contrato inmobiliario" className="servicios_home_img" />
-                        <span className="imgText">Contratos</span>
-                    </Link>
+                <div className="servicios_home_imgContainer" onClick={() => scrollToSection('contratos')}>
+                    <img src="./contrato.jpg" alt="contrato inmobiliario" className="servicios_home_img" />
+                    <span className="imgText">Contratos</span>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
